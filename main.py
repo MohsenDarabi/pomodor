7ab82343bb4247f6f8e4953ff704b5479ac7e468
Mohsen Darabi <38433815+MohsenDarabi@users.noreply.github.com>
2024-07-12 22:52:31 +0200

    add timer lable

--- main.py
+++ main.py
@@ -20,12 +20,13 @@ LONG_BREAK_MIN = 20
 window = Tk()
 window.title("Pomodor")
 window.config(padx=100, pady=50, bg=YELLOW)
-
+timer_label = Label(window, text="Timer", font=(FONT_NAME, 35,"bold"),foreground=GREEN, bg=YELLOW)
+timer_label.grid(column=1, row=0)
 canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_img= PhotoImage(file="tomato.png")
 canvas.create_image(100, 112, image=tomato_img)
 canvas.create_text(100,130, text="00:00", fill="white", font=(FONT_NAME, 35,"bold"), anchor=CENTER)
-canvas.pack()
+canvas.grid(column=1, row=1, columnspan=2)
 
 
 
