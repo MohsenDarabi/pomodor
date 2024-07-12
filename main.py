431932eba7e2d1053c9aac96580b001184f30df5
Mohsen Darabi <38433815+MohsenDarabi@users.noreply.github.com>
2024-07-12 22:28:00 +0200

    get rid of the edge / border of the canvas image

--- main.py
+++ main.py
@@ -21,10 +21,10 @@ window = Tk()
 window.title("Pomodor")
 window.config(padx=100, pady=50, bg=YELLOW)
 
-canvas = Canvas(width=200, height=224, bg=YELLOW)
+canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_img= PhotoImage(file="tomato.png")
-canvas.create_image(103, 112, image=tomato_img)
-canvas.create_text(103,130, text="00:00", fill="white", font=(FONT_NAME, 35,"bold"), anchor=CENTER)
+canvas.create_image(100, 112, image=tomato_img)
+canvas.create_text(100,130, text="00:00", fill="white", font=(FONT_NAME, 35,"bold"), anchor=CENTER)
 canvas.pack()
 
 
