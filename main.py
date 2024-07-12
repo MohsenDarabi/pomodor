de73d0dde28457b658b91ae0e75ffb7725a2db88
Mohsen Darabi <38433815+MohsenDarabi@users.noreply.github.com>
2024-07-12 23:02:32 +0200

    buttons and check label

--- main.py
+++ main.py
@@ -20,8 +20,17 @@ LONG_BREAK_MIN = 20
 window = Tk()
 window.title("Pomodor")
 window.config(padx=100, pady=50, bg=YELLOW)
+
 timer_label = Label(window, text="Timer", font=(FONT_NAME, 35,"bold"),foreground=GREEN, bg=YELLOW)
 timer_label.grid(column=1, row=0)
+start_button = Button(window, text="Start", font=(FONT_NAME, 15,"bold"), width="5")
+start_button.grid(column=0, row=2)
+
+reset_button = Button(window, text="Reset", font=(FONT_NAME, 15,"bold"), width="5")
+reset_button.grid(column=3, row=2)
+
+checked_label =Label(text=" ✓", font=(FONT_NAME, 30,"bold"), width="5",foreground=GREEN, bg=YELLOW)
+checked_label.grid(column=1, row=3)
 canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_img= PhotoImage(file="tomato.png")
 canvas.create_image(100, 112, image=tomato_img)
