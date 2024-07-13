dafc21da53483bac4d25031266f9cf847406ca42
Mohsen Darabi <38433815+MohsenDarabi@users.noreply.github.com>
2024-07-13 17:08:05 +0200

    check marks

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ RED = "#e7305b"
 GREEN = "#9bdeac"
 YELLOW = "#f7f5dd"
 FONT_NAME = "Courier"
-WORK_MIN = 25
+WORK_MIN = 1
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
 reps =0
@@ -49,6 +49,11 @@ def count_down(count):
         window.after(1000, count_down, count -1)
     else:
         start_timer()
+        marks = ""
+        for _ in range(math.floor(reps/2)):
+            marks +="✓"
+        checked_label.config(text=marks, font=(FONT_NAME, 30,"bold"), width="5",)
+
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
 window.title("Pomodor")
@@ -63,7 +68,7 @@ start_button.grid(column=0, row=2)
 reset_button = Button(window, text="Reset", font=(FONT_NAME, 15,"bold"), width="5")
 reset_button.grid(column=3, row=2)
 
-checked_label =Label(text=" ✓", font=(FONT_NAME, 30,"bold"), width="5",foreground=GREEN, bg=YELLOW)
+checked_label =Label(foreground=GREEN, bg=YELLOW)
 checked_label.grid(column=1, row=3)
 canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_img= PhotoImage(file="tomato.png")
