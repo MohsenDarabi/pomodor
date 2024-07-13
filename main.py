fb0f6007944c290fe2486bdc45105f4d0303493b
Mohsen Darabi <38433815+MohsenDarabi@users.noreply.github.com>
2024-07-13 16:55:46 +0200

    modify timer text to dynamically change according

--- main.py
+++ main.py
@@ -10,18 +10,35 @@ FONT_NAME = "Courier"
 WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
+reps =0
 
 # ---------------------------- TIMER RESET ------------------------------- # 
 
 # ---------------------------- TIMER MECHANISM ------------------------------- #
 def start_timer():
+    global reps
+    reps += 1
 
-    count_down(5*60)
+    work_sec = WORK_MIN * 60
+    short_break_sec = SHORT_BREAK_MIN * 60
+    long_break_sec = LONG_BREAK_MIN * 60
+
+    if reps % 8 == 0:
+        timer_label.config(text=f"long Brake")
+        count_down(long_break_sec)
+    elif reps % 2 == 0:
+        timer_label.config(text=f"Short Brake")
+        count_down(short_break_sec)
+    else:
+        timer_label.config(text=f"Working Time")
+        count_down(work_sec)
+    reps += 1
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
 def count_down(count):
     count_min = math.floor(count / 60)
     count_sec = count % 60
+
     if count_sec < 10:
         # count_sec = "0" + str(count_sec)
         count_sec =f"0{count_sec}"
@@ -30,6 +47,8 @@ def count_down(count):
     canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
     if count > 0:
         window.after(1000, count_down, count -1)
+    else:
+        start_timer()
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
 window.title("Pomodor")
